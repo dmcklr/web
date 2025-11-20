39d5baecdf0c7be577fdc0ec32ce91028bf7f7f3
dennis <meckler@raceresult.com>
2025-11-20 21:34:02 +0100

    update pwa

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,20 +1,19 @@
-self.addEventListener("install", event => {
-event.waitUntil(
-caches.open("pwa-cache-v1").then(cache => {
-return cache.addAll([
-"/",
-"/index.html",
-"/manifest.json"
-]);
-})
-);
+const CACHE_NAME = 'pwa-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-
-self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request);
-})
-);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
