94505805cb34ca690351574130c0a1a528e73174
dmcklr <179860546+dmcklr@users.noreply.github.com>
2026-06-08 09:56:17 +0200

    change events

--- led/events.js
+++ led/events.js
@@ -4,30 +4,30 @@ const events = [
         text1: "London Marathon 2026",
         text2: "1:59:30 – World Record",        
     },
+        {
+        id: "erzberg",       
+        text1: "Erzberg Rodeo",        
+        text2: "Toughest enduro race worldwide",       
+    },
     {
         id: "vancouver",       
         text1: "Vancouver Sun Run",
-        text2: "57,000 runners",
-        
-    },
+        text2: "57,000 runners",        
+    }, 
+
     {
         id: "capeepic",       
         text1: "Cape Epic ",
         text2: "World's most famous stage race",        
-    },       
-
-
-    {
-        id: "erzberg",       
-        text1: "Erzberg Rodeo",        
-        text2: "Toughest enduro race worldwide",       
     }
+    
 /*
     {
         id: "marbella",       
         text1: "Ironman 70.3",
         text2: "World Championship Marbella",        
     },
+
 
     // Weitere Events können hier hinzugefügt werden
     */
