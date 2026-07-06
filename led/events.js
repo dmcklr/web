2e65f3ce11241617e8544af6ec3a678f0e09dd06
dennis <meckler@raceresult.com>
2026-07-06 07:49:37 +0200

    add roth to led

--- led/events.js
+++ led/events.js
@@ -4,24 +4,25 @@ const events = [
         text1: "London Marathon 2026",
         text2: "1:59:30 – World Record",        
     },
-        {
+
+    {
+        id: "roth",       
+        text1: "Challenge Roth 2026",
+        text2: "7:21:04 - World Best Time",        
+    }, 
+/*
+    {
         id: "erzberg",       
         text1: "Erzberg Rodeo",        
         text2: "Toughest enduro race worldwide",       
     },
-    {
-        id: "vancouver",       
-        text1: "Vancouver Sun Run",
-        text2: "57,000 runners",        
-    }, 
-
     {
         id: "capeepic",       
         text1: "Cape Epic ",
         text2: "World's most famous stage race",        
     }
     
-/*
+
     {
         id: "marbella",       
         text1: "Ironman 70.3",
